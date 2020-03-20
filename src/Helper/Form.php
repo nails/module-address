da1c5f7887f3d7f2978d77baa4bc4d49b1721396
Pablo de la Peña <p@shedcollective.org>
2020-03-20 00:46:27 +0000

    Admin markup

--- src/Helper/Form.php
+++ src/Helper/Form.php
@@ -2,7 +2,8 @@
 
 namespace Nails\Address\Helper;
 
-use Nails\Common\Service\Country;
+use Nails\Common\Exception\FactoryException;
+use Nails\Common\Service;
 use Nails\Factory;
 
 /**
@@ -12,9 +13,17 @@ use Nails\Factory;
  */
 class Form
 {
+    /**
+     * Renders an address form field
+     *
+     * @param array $aField The fields config array
+     *
+     * @return string
+     * @throws FactoryException
+     */
     public static function address(array $aField)
     {
-        /** @var Country $oCountryService */
+        /** @var Service\Country $oCountryService */
         $oCountryService = Factory::service('Country');
 
         $aField['class'] .= 'js-address-picker loading';
@@ -27,15 +36,43 @@ class Form
                     $aField['default']
                 ),
                 '<div class="js-address-picker__fields">',
-                form_input('', '', ' class="js-address-picker__label" placeholder="Label"'),
-                form_input('', '', ' class="js-address-picker__line_1" placeholder="Line 1"'),
-                form_input('', '', ' class="js-address-picker__line_2" placeholder="Line 2"'),
-                form_input('', '', ' class="js-address-picker__line_3" placeholder="Line 3"'),
-                form_input('', '', ' class="js-address-picker__town" placeholder="Town"'),
-                form_input('', '', ' class="js-address-picker__region" placeholder="Region"'),
-                form_input('', '', ' class="js-address-picker__postcode" placeholder="Postcode"'),
-                form_dropdown(
+                form_input(
+                    $aField['key'] . '_data[label]',
                     '',
+                    'class="js-address-picker__label" placeholder="Label"'
+                ),
+                form_input(
+                    $aField['key'] . '_data[line_1]',
+                    '',
+                    'class="js-address-picker__line_1" placeholder="Line 1"'
+                ),
+                form_input(
+                    $aField['key'] . '_data[line_2]',
+                    '',
+                    'class="js-address-picker__line_2" placeholder="Line 2"'
+                ),
+                form_input(
+                    $aField['key'] . '_data[line_3]',
+                    '',
+                    'class="js-address-picker__line_3" placeholder="Line 3"'
+                ),
+                form_input(
+                    $aField['key'] . '_data[town]',
+                    '',
+                    'class="js-address-picker__town" placeholder="Town"'
+                ),
+                form_input(
+                    $aField['key'] . '_data[region]',
+                    '',
+                    'class="js-address-picker__region" placeholder="Region"'
+                ),
+                form_input(
+                    $aField['key'] . '_data[postcode]',
+                    '',
+                    'class="js-address-picker__postcode" placeholder="Postcode"'
+                ),
+                form_dropdown(
+                    $aField['key'] . '_data[country]',
                     ['' => 'Select a country'] + $oCountryService->getCountriesFlat(),
                     '',
                     'class="select2 js-address-picker__country" placeholder="Country"'
