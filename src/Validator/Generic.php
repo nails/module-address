9e64533cb71f0be1e2201eeb69c4dbc6ba8fb60d
Pablo de la Peña <p@shedcollective.org>
2021-02-05 16:09:19 +0000

    Use FormValidator to validate address

--- src/Validator/Generic.php
+++ src/Validator/Generic.php
@@ -2,12 +2,12 @@
 
 namespace Nails\Address\Validator;
 
-use Nails\Address\Exception\Validation\InvalidCountryException;
 use Nails\Address\Interfaces\Validator;
 use Nails\Address\Resource\Address;
 use Nails\Common\Exception\FactoryException;
 use Nails\Common\Exception\ValidationException;
 use Nails\Common\Service\Country;
+use Nails\Common\Service\FormValidation;
 use Nails\Factory;
 
 /**
@@ -21,41 +21,37 @@ class Generic implements Validator
      * Validates an address object
      *
      * @param Address $oAddress The Address to validate
+     * @param array   $aRules   Any additional validation rules to apply
      *
      * @throws FactoryException
-     * @throws InvalidCountryException
+     * @throws ValidationException
      */
-    public static function validate(Address $oAddress): void
+    public static function validate(Address $oAddress, array $aRules = []): void
     {
-        static::validateCountry($oAddress);
-    }
-
-    // --------------------------------------------------------------------------
-
-    /**
-     * @param Address $oAddress The Address to validate
-     *
-     * @throws InvalidCountryException
-     * @throws FactoryException
-     */
-    protected static function validateCountry(Address $oAddress): void
-    {
-        if (empty($oAddress->country)) {
-            throw new InvalidCountryException(
-                'Missing country property'
-            );
-        }
-
+        /** @var FormValidation $oFormValidation */
+        $oFormValidation = Factory::service('FormValidation');
         /** @var Country $oCountryService */
         $oCountryService = Factory::service('Country');
-        $oCountry        = $oCountryService->getCountry($oAddress->country);
-        if (empty($oCountry)) {
-            throw new InvalidCountryException(
-                sprintf(
-                    '"%s" is not a valid country',
-                    $oAddress->country
-                )
-            );
-        }
+
+        $oFormValidation
+            ->buildValidator(array_merge(
+                [
+                    'line_1'   => [
+                        FormValidation::RULE_REQUIRED,
+                    ],
+                    'postcode' => [
+                        FormValidation::RULE_REQUIRED,
+                    ],
+                    'country'  => [
+                        FormValidation::RULE_REQUIRED,
+                        FormValidation::rule(
+                            FormValidation::RULE_IN_LIST,
+                            implode(',', array_keys($oCountryService->getCountriesFlat()))
+                        ),
+                    ],
+                ],
+                $aRules
+            ))
+            ->run($oAddress->formatted()->asArray());
     }
 }
